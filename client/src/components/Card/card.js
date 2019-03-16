004741e856f666b1ca9f3259133c81c5e51010b2
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-03-15 20:16:06 -0700

    added props

--- client/src/components/Card/card.js
+++ client/src/components/Card/card.js
@@ -1,22 +1,23 @@
 import React from "react";
 import "./card.css";
 
-function Card(props) {
+function Card (title, subtitle, authors, link, description, image, handleBookSave) {
 
   return (
 
     <div className="card border border-success mb-3">
       <div className="row no-gutters">
         <div className="col-md-4">
-          <img alt={props.name} src={props.image} />
+          <img alt={title} src={image} />
         </div>
         <div className="col-md-8">
           <div className="card-body">
-            <h5 className="card-title">Book title</h5>
-            <p className="card-text">Authors: </p>
-            <p className="card-text">Description: </p>
-            <a href="#" className="btn btn-sm btn-dark">View</a>
-            <a href="#" className="btn btn-sm btn-dark">Save</a>
+            <h5 className="card-title">{title}</h5>
+            {subtitle && <h6 className="font-italic">{subtitle}</h6>}
+            <p className="card-text">Authors: {authors}</p>
+            <p className="card-text">Description: {description}</p>
+            <a href={link} target="_blank" className="btn btn-sm btn-dark">View</a>
+            <a href="#" onClick={handleBookSave} className="btn btn-sm btn-dark">Save</a>
           </div>
         </div>
       </div>
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -4,7 +4,58 @@ import SearchBox from "../components/Search/searchBox"
 import Main from "../components/Main/main";
 import Row from "../components/Row/row";
 import Card from "../components/Card/card";
+import API from "../utils/API";
+import { runInThisContext } from "vm";
 
+class Saved extends Component {
+    state = {
+        books: [],
+        searchTerm: "",
+        message: "Please, search for a book"
+    };
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    getBooks = () => {
+        API.getBooks(this.state.searchTerm)
+            .then(res =>
+                this.setState({
+                    books: res.data
+                })
+            )
+            .catch(() =>
+                this.setState({
+                    books: [],
+                    message: "We couldn't find book that matched search criteria. Please, try again with differnt search term."
+                })
+            );
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.getBooks();
+    };
+
+    handleBookSave = id => {
+        const book = this.state.books.find(book => book.id === id);
+
+        API.saveBook({
+            googleId: book.id,
+            tite: book.volumeInfo.title,
+            subtitle: book.volumeInfo.subtitle,
+            link: book.volumeInfo.infoLink,
+            authors: book.volumeInfo.authors,
+            description: book.volumeInfo.description,
+            image: book.volumeInfo.imageLinks.thumbnail
+        })
+            .then(() => this.getBooks());
+    }
+}
 
 function Search() {
     return (
@@ -12,10 +63,31 @@ function Search() {
             <Title />
             <Main>
                 <Row>
-                    <SearchBox />
+                    <SearchBox
+                        handleInputChange={this.handleInputChange}
+                        handleFormSubmit={this.handleFormSubmit}
+                        searchTerm={this.state.searchTerm}
+                    />
                 </Row>
                 <Row> Results
-            <Card />
+                {this.state.books.length ? (
+                    <div>
+                        {this.state.books.map(book =>(
+                            <Card 
+                                key={book.id}
+                                title={book.volumeInfo.title}
+                                subtitle={book.volumeInfo.subtitle}
+                                link={book.volumeInfo.infoLink}
+                                authors={book.volumeInfo.authors.join(", ")}
+                                description={book.volumeInfo.description}
+                                image={book.volumeInfo.imageLinks.thumbnail}
+                                handleBookSave= {this.handleBookSave(book.id)}
+                            />
+                        ))}
+                        </div>
+                        ) : (
+                            <h2 className="text-center">{this.state.message}</h2>
+                )}
                 </Row>
             </Main>
         </div>
