dec81716a984a3506c819d569adf4cb2ff82d090
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-03-18 17:44:33 -0700

    updates

--- server.js
+++ server.js
@@ -13,15 +13,12 @@ app.use(routes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'));
-  })
-
 }
 
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
